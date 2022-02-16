d7ae4f95963352d5a9a94a2455a7e30c2847bcae
Santiago <326sdg5@gmail.com>
2022-02-15 23:33:11 -0600

    Removed contact form functionality

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
-import smtplib
+# import smtplib
 
 
 app = Flask(__name__)
@@ -191,22 +191,22 @@ def about():
 
 
 @app.route("/contact", methods=["GET", "POST"])
-# def contact():
-#     return render_template("contact.html", current_user=current_user)
-def receive_data():
-    if request.method == "POST":
-        data = request.form
-        send_email(data['name'], data['email'], data['number'], data['message'])
-        return render_template("contact.html", msg_sent=True, current_user=current_user)
-    return render_template("contact.html", msg_sent=False, current_user=current_user)
-
-
-def send_email(name, email, phone, message):
-    email_message = f"Subject:Message from Blog\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(OWN_EMAIL, OWN_PASSWORD)
-        connection.sendmail(OWN_EMAIL, OWN_PASSWORD, email_message)
+def contact():
+    return render_template("contact.html", current_user=current_user)
+# def receive_data():
+#     if request.method == "POST":
+#         data = request.form
+#         send_email(data['name'], data['email'], data['number'], data['message'])
+#         return render_template("contact.html", msg_sent=True, current_user=current_user)
+#     return render_template("contact.html", msg_sent=False, current_user=current_user)
+#
+#
+# def send_email(name, email, phone, message):
+#     email_message = f"Subject:Message from Blog\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+#     with smtplib.SMTP("smtp.gmail.com") as connection:
+#         connection.starttls()
+#         connection.login(OWN_EMAIL, OWN_PASSWORD)
+#         connection.sendmail(OWN_EMAIL, OWN_PASSWORD, email_message)
 
 
 @app.route("/new-post", methods=["GET", "POST"])
