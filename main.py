d70975d5591b86bec1b5c91402f9fb629df7e288
Santiago <326sdg5@gmail.com>
2022-02-15 23:42:31 -0600

    Revised edit form

--- main.py
+++ main.py
@@ -236,7 +236,8 @@ def edit_post(post_id):
         title=post.title,
         subtitle=post.subtitle,
         img_url=post.img_url,
-        author=post.author,
+        # author=post.author,
+        author=current_user,
         body=post.body
     )
     if edit_form.validate_on_submit():
